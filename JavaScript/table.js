79a95ea36a2e0b37579d3a5617f33e409cc97075
phanindra <phanindra.jaya@mobigesture.com>
2022-04-08 11:18:45 -0800

    Bootstrap added

--- JavaScript/table.js
+++ JavaScript/table.js
@@ -4,8 +4,6 @@ fetch("http://ec2-52-87-201-103.compute-1.amazonaws.com:5000/get_all_profiles")
     return res.json();
   })
   .then((json) => {
-    //   data.push(json);
-    // console.log(json);
     var heading_row = document.createElement("tr");
     for (var k in json[0]) {
       if (
@@ -30,10 +28,7 @@ fetch("http://ec2-52-87-201-103.compute-1.amazonaws.com:5000/get_all_profiles")
     }
     document.getElementById("table_heading_row").appendChild(heading_row);
 
-    console.log(json);
-
     for (let i = 0; i < json.length; i++) {
-      //   console.log(json[i]);
       var body_row = document.createElement("tr");
       for (let k in json[i]) {
         if (
@@ -54,25 +49,12 @@ fetch("http://ec2-52-87-201-103.compute-1.amazonaws.com:5000/get_all_profiles")
         } else {
           var table_data = document.createElement("td");
           table_data.innerHTML = json[i][k];
-          //   console.log(table_data);
         }
         body_row.appendChild(table_data);
       }
-      //   console.log(body_row[i].length);
       document.getElementById("table-body").appendChild(body_row);
     }
-    console.log(document.getElementById("table-body"));
-    // console.log(document.getElementById("table-body"));
-    // json.map((values) => {
-    //   data += `<tr>
-    //     <td>John</td>
-    //     <td>${json[i].email}</td>
-    //     <td>john@example.com</td>
-    // </tr>`;
-    // });
-    // document.getElementById("table-body").innerHTML = data;
   })
   .catch((err) => {
     console.log(err);
   });
-console.log(data);
